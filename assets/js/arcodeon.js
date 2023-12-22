e614d4c0147632136fe340d0dcf93b8daf734dd1
Valclemir Soares Dos Reis <valclemir88@gmail.com>
2023-12-21 22:44:27 -0300

    first commit

---
+++ assets/js/arcodeon.js
@@ -0,0 +1,13 @@
+const arcodeonTrigger=document.querySelectorAll(".arcodeon .trigger")
+
+arcodeonTrigger.forEach((trigger)=>{
+    trigger.addEventListener('click',(e)=>{
+        const arcodeon= trigger.parentElement
+        const isOpen= arcodeon.classList.contains("open")
+        if(isOpen){
+            arcodeon.classList.remove('open')
+        }else{
+            arcodeon.classList.add('open')
+        }
+    })
+})
