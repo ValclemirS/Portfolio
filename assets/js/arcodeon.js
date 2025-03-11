ad45c4e41ef798bbf5722200d9fdd8eebdc7bf83
Valclemir Soares Dos Reis <135272998+ValclemirS@users.noreply.github.com>
2025-03-10 22:22:03 -0300

    update

--- assets/js/arcodeon.js
+++ assets/js/arcodeon.js
@@ -1,13 +1,14 @@
-const arcodeonTrigger=document.querySelectorAll(".arcodeon .trigger")
+const arcodeonTrigger = document.querySelectorAll(".arcodeon .trigger");
 
-arcodeonTrigger.forEach((trigger)=>{
-    trigger.addEventListener('click',(e)=>{
-        const arcodeon= trigger.parentElement
-        const isOpen= arcodeon.classList.contains("open")
-        if(isOpen){
-            arcodeon.classList.remove('open')
-        }else{
-            arcodeon.classList.add('open')
+arcodeonTrigger.forEach((trigger) => {
+    trigger.addEventListener('click', (e) => {
+        const arcodeon = trigger.parentElement;
+        const isOpen = arcodeon.classList.contains("open");
+
+        if (isOpen) {
+            arcodeon.classList.remove('open');
+        } else {
+            arcodeon.classList.add('open');
         }
-    })
-})
+    });
+});
